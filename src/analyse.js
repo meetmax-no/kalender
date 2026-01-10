a746f18c1509217e1a5da8f620bb4cd46f009198
meetmax-no <firma@meetmax.no>
2026-01-10 14:11:01 +0100

    Create analyse.js

---
+++ src/analyse.js
@@ -0,0 +1,7 @@
+window.AnalyseDashboard = () => {
+    return (
+        <div className="p-10 text-center bg-white rounded-xl shadow-sm border border-slate-200">
+            <h1 className="text-2xl font-bold text-slate-700">Velkommen til analyse!</h1>
+        </div>
+    );
+};
