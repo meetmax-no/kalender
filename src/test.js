e8084c8ed950ad5fc790f52c212b7cfd57437969
meetmax-no <firma@meetmax.no>
2026-03-17 14:20:19 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -3,3 +3,8 @@
     //'FB_BOOST': { label: 'FB Boost (Ads)', icon: 'megaphone', color: 'text-green-600', bg: 'bg-green-100', defaultTime: '12:00' },
     //'IG_POST': { label: 'Instagram', icon: 'instagram', color: 'text-pink-600', bg: 'bg-pink-100', defaultTime: '18:00' },
     //'DM': { label: 'Nyhetsbrev', icon: 'mail', color: 'text-amber-600', bg: 'bg-amber-100', defaultTime: '14:00' },
+
+    'FB_ORG': { label: 'FB Organisk', icon: 'facebook', color: 'text-blue-600', bg: 'bg-blue-100', defaultTime: '10:00' },
+    'FB_BOOST': { label: 'FB Boost (Ads)', icon: 'megaphone', color: 'text-green-600', bg: 'bg-green-100', defaultTime: '12:00' },
+    'IG_POST': { label: 'Instagram', icon: 'instagram', color: 'text-pink-600', bg: 'bg-pink-100', defaultTime: '18:00' },
+    'DM': { label: 'Nyhetsbrev', icon: 'mail', color: 'text-amber-600', bg: 'bg-amber-100', defaultTime: '14:00' }
